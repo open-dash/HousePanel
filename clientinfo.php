e6e8aa540ed2c90efa14d0bfa1c72a67cadd6bca
Ken Washington <ken@kenw.com>
2017-07-15 22:21:31 -0400

    added template file for clientinfo.php

---
+++ clientinfo.php
@@ -0,0 +1,4 @@
+<?php
+define('CLIENT_ID', 'abc1abc1-abc2-abc3-abc4-abc5abc5abc5');
+define('CLIENT_SECRET', 'abc1abc1-abc2-abc3-abc4-abc5abc5abc5');
+define('ST_WEB','https://graph.api.smartthings.com');
