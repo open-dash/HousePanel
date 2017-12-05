21453aa89b19d5ec03420c458a9886623c3a2e0a
Ken Washington <ken@kenw.com>
2017-12-04 20:41:35 -0500

    updated phpinfo to print server info up front

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,8 @@
 <?php
 // simple file to display the configuration info for PHP
+// echo "https: = " . ($_SERVER['HTTPS'] ? $_SERVER['HTTPS'] : "NONE") . "<br />";
+echo "scheme = " . $_SERVER['REQUEST_SCHEME'] . "<br />";
+echo "name   = " . $_SERVER['SERVER_NAME'] . "<br />";
+echo "self   = " . $_SERVER['PHP_SELF'] . "<br />";
+echo "uri    = " . $_SERVER['REQUEST_URI'] . "<br /><br />";
 phpinfo();
